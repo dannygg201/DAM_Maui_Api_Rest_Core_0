d33c1c265220a87760a95802351916b0f08acc9d
Danny González Silva <Dannygg201@outlook.es>
2025-06-11 11:34:10 -0600

    Version_01: capa Api, capa entidad y prueba de dependencias

---
+++ CapaEntidad/PersonaCLS.cs
@@ -0,0 +1,18 @@
+﻿namespace CapaEntidad
+{
+    public class PersonaCLS
+    {
+        //Listado
+        public int iidpersona { get; set; }
+        public string? nombrecompleto { get; set; }
+        public string correo { get; set; }
+        public string fechanacimientocadena { get; set; }
+        public int iidsexo { get; set; }
+
+        // 
+        public string nombre { get; set; }
+        public string appaterno { get; set; }
+        public string apmaterno { get; set; }
+        public DateTime fechanacimiento { get; set; }
+    }
+}
---
+++ Web.Api/Controllers/PersonaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CapaEntidad;
+using Web.Api.Models;
+using System.Linq.Expressions;
+//DbAba2d0BdveterinariaContext
+namespace Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet("{nombrecompleto}")]
+        public List<PersonaCLS> listarPersona(string nombrecompleto)
+        {
+            List<PersonaCLS> lista = new List<PersonaCLS>();
+
+            try
+            {
+                using (DbAba2d0BdveterinariaContext bd = new DbAba2d0BdveterinariaContext())
+                {
+                    lista = (from persona in bd.Personas
+                             where persona.Bhabilitado == 1
+                             && (persona.Nombre + " " + persona.Appaterno + " " + persona.Apmaterno).Contains(nombrecompleto)
+                             select new PersonaCLS
+                             {
+                                 iidpersona = persona.Iidpersona,
+                                 nombrecompleto = persona.Nombre + " " + persona.Appaterno + " " + persona.Apmaterno,
+                                 correo = persona.Correo,
+                                 fechanacimientocadena = persona.Fechanacimiento == null ? "" :
+                                 persona.Fechanacimiento.Value.ToShortDateString()
+                             }).ToList();
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                return lista;
+            }
+        }
+    }
+}
+
